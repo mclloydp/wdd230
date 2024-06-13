0f11c5737d07aa6d881c4d6121798468f56510cd
McLloyd Pise <mclloydp@byui.edu>
2024-06-13 03:03:20 +0200

    Created the required files and folders in the root directory

--- scripts/date.js
+++
@@ -1,5 +0,0 @@
-const options = {
-    month: "short",
-    day: "numeric",
-    year: "numeric"
-};       document.getElementById('lastupdated').textContent = new Date(document.lastModified).toLocaleDateString("en-US", options); 

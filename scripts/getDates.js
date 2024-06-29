5c06f1b13ecbc1042e00bf7777aed42878792060
McLloyd Pise <mclloydp@byui.edu>
2024-06-29 03:59:15 +0200

    I made sevaral changies, additions and removals. Its a job in progress.

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const lastModifiedSpan = document.getElementById('lastModified');
     lastModifiedSpan.textContent = `Last Modification: ${document.lastModified}`;
 });
+
+// toLocaleDateString 
+const options = {weekday: 'long',day: 'numeric', month: 'long', year: 'numeric', time: 'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
